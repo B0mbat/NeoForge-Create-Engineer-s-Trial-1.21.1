1613b25a3d28f5b7476256bab958a5910783250d
LeoT0815 <leo.tuechler@bg2000.at>
2026-07-24 13:57:10 +0200

    Minecraft Crash Fixed 2

--- src/main/java/net/sheep/create_engineers_trial/item/ModItems.java
+++ src/main/java/net/sheep/create_engineers_trial/item/ModItems.java
@@ -1,12 +1,21 @@
 package net.sheep.create_engineers_trial.item;
 
 import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.sheep.create_engineers_trial.CreateEngineersTrial;
 
 public class ModItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CreateEngineersTrial.MOD_ID);
-    public static final DeferredItem<Item> EMPTY_SUPER_GLUE = ITEMS.register("empty_super_glue",
-            () -> new Item(new Item.Properties()));
+
+    public static final DeferredRegister.Items ITEMS =
+            DeferredRegister.createItems(CreateEngineersTrial.MOD_ID);
+
+    public static final DeferredItem<Item> EMPTY_SUPER_GLUE =
+            ITEMS.register("empty_super_glue",
+                    () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
