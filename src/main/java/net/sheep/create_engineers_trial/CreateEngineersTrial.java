19a59a81b3c1ea97dc8f3af26a2cbf58a92774de
LeoT0815 <leo.tuechler@bg2000.at>
2026-08-06 19:50:08 +0200

    hi

--- src/main/java/net/sheep/create_engineers_trial/CreateEngineersTrial.java
+++ src/main/java/net/sheep/create_engineers_trial/CreateEngineersTrial.java
@@ -1,7 +1,6 @@
 package net.sheep.create_engineers_trial;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -9,7 +8,6 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.sheep.create_engineers_trial.block.ModBlocks;
 import net.sheep.create_engineers_trial.fluid.ModFluidTypes;
@@ -36,6 +34,8 @@ public class CreateEngineersTrial {
 
         NeoForge.EVENT_BUS.register(this);
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
